8551549b7035ee2245be4096d51bd45139fdabbb
z4122 <412213484@qq.com>
2018-06-04 20:11:20 +0800

    K-th Symbol in Grammar

---
+++ K-th Symbol in Grammar.c
@@ -0,0 +1,20 @@
+int kthGrammar(int N, int K) {
+        int order = 1;
+        int count = 0;
+        while(order<K)
+            order *= 2;
+        while(K>2)
+        {
+            order = order/2;
+            if (K > order)
+		{
+			K = K - order;
+			count++;
+		}      
+        }   
+        if(count%2)
+            K = 2-K;
+        else
+            K = K - 1;
+        return K;
+    }
