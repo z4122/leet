764325c95e91a0afe4437ae75698d7c5dfa4a16d
z4122 <412213484@qq.com>
2018-08-17 11:42:57 +0800

    add recursion

--- string2num.c
+++ string2num.c
@@ -56,3 +56,62 @@ int main()
 	system("pause");
 	return 0;
 }
+
+//下面是递归的方法
+
+// ConsoleApplication2.cpp: 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include "stdio.h"
+
+#include "iostream"
+
+using namespace std;
+#define Bnd(X,bnd) (sizeof(X)+(bnd)&~(bnd))
+
+
+
+void print_num(string tmp)
+{
+	int n = 0;
+	while (tmp[n] == '0')
+		n++;
+	for(int i = n;i<tmp.size();i++)
+			printf("%c", tmp[i]);
+	printf("\n");
+	
+}
+
+void print_re(string &tmp, int n)
+{
+	if (n == tmp.size())
+		print_num(tmp);
+	else
+	{
+		for (int i = 0; i < 10; i++)
+		{
+			tmp[n] = '0' + i;
+			print_re(tmp, n + 1);
+		}
+	}
+
+}
+
+void Print(int n)
+{
+	if (n < 0)
+		return;
+	string tmp(n, '0');
+
+	print_re(tmp, 0);
+	
+
+}
+
+int main()
+{
+	Print(3);
+	system("pause");
+	return 0;
+}
