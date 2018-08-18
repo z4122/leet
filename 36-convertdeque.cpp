ba48445faac945dcb9d9ab050b8d92c872da9c55
z4122 <412213484@qq.com>
2018-08-18 17:40:28 +0800

    36-convertdeque

---
+++ 36-convertdeque.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+BinaryTreeNode* Convert(BinaryTreeNode* pRootOfTree)
+{
+    BinaryTreeNode* pLastNodeInList = nullptr;
+    ConvertNode(pRootOfTree,&pLastNodeInList);
+
+
+
+}
+
+
+void ConvertNode(BinaryTreeNode* root,BinaryTreeNode ** pLastNodeInList)
+{
+    BinaryTreeNode* current = root;
+    if(root->left!=nullptr)
+        ConvertNode(root->left,pLastNodeInList);
+    
+    current->left = *pLastNodeInList;
+    (*pLastNodeInList)->right = current;
+    *pLastNodeInList = current;
+
+    if(root->right!=nullptr)
+        ConvertNode(root->right,pLastNodeInList);
+
+}
